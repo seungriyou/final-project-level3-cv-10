c6cc09f0207662dad6976175dddddd201cdb6649
seungriyou <seungri0826@gmail.com>
2022-06-15 00:36:27 +0900

    [Feat] Add .env files

--- serving/frontend/src/components/Message.js
+++ serving/frontend/src/components/Message.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import { FACE_API } from '../config';
 
 export default function Message() {
 
     const [result, setResult] = useState(null);
     const message = async () => {
         try {
-            let res = await axios.get(`${FACE_API}/`);
+            let res = await axios.get(`${process.env.REACT_APP_FACE_API}/`);
             let result = res.data["message"];
             setResult(result);
         } catch(e) {
